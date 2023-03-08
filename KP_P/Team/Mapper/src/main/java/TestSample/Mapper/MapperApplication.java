3ad0655b8d7de8c6b0b234445b129deb59e560d9
cycotest <aceldamadk@gamil.com>
2023-03-08 14:32:24 +0900

    gradle default

---
+++ KP_P/Team/Mapper/src/main/java/TestSample/Mapper/MapperApplication.java
@@ -0,0 +1,13 @@
+package TestSample.Mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperApplication.class, args);
+	}
+
+}
