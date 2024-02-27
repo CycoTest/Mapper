e22c28f5ea99c392b4467579e7c64f27913c1a11
CycoTest <aceldamadk@gmail.com>
2024-02-27 18:54:02 +0900

    초기 커밋 메시지

---
+++ Maper/src/main/java/project/personal/Maper/MaperApplication.java
@@ -0,0 +1,13 @@
+package project.personal.Maper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaperApplication.class, args);
+	}
+
+}
