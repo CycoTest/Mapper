881727e1b0cbe7cd4c0e3ac9d300bd25afc054c7
CycoTest <aceldamadk@gmail.com>
2024-02-27 20:29:37 +0900

    First Commit

---
+++ src/test/java/project/personal/Maper/MaperApplicationTests.java
@@ -0,0 +1,13 @@
+package project.personal.Maper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
